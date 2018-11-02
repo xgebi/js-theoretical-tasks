2e3e5923c8161f302c9cdd2982065c4a662f5790
xgebi <sarah@sarahgebauer.com>
2018-11-02 07:23:59 +0100

    removed redundant while loop

--- 01-countUnique/countUnique.js
+++ 01-countUnique/countUnique.js
@@ -2,18 +2,13 @@ function countUniqueValues(arr) {
   let unique = [];
   let i = 0;
   unique.push(arr[i]);
-  while (i < arr.length) {
-    let j = i + 1;
-    while (j < arr.length) {
-      if (arr[i] === arr[j]) {
-        j++;
-      } else {
-        unique.push(arr[j]);
-        i = j++;
-      }
-    }
-    if (j === arr.length) {
-      i = arr.length;
+  let j = i + 1;
+  while (j < arr.length) {
+    if (arr[i] === arr[j]) {
+      j++;
+    } else {
+      unique.push(arr[j]);
+      i = j++;
     }
   }
   return unique;
